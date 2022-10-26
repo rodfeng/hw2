f74f88193afb2e110ac7cf148b54042d996baeba
Roderick Feng <roderickfeng@Rodericks-MBP.attlocal.net>
2022-10-26 02:12:11 -0700

    completed hw6

---
+++ hw6/Shopping List 2/Shopping List 2/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  Shopping List 2
+//
+//  Created by Tony Hong on 3/27/22.
+//
+
+import SwiftUI
+
+class Items: Identifiable {
+    
+    var itemName: String
+    
+    var quantity: String
+    
+    init(itemName: String, quantity: String) {
+        
+        self.itemName = itemName
+        
+        self.quantity = quantity
+    }
+    
+}
+
+struct ContentView: View {
+    @State var tempItemName: String = ""
+    @State var tempQuantity: String = ""
+    @State var list = [
+        Items(itemName: "Bananas", quantity: "3"),
+        Items(itemName: "Apples", quantity: "4"),
+        Items(itemName: "Eggs", quantity: "12")
+    ]
+        
+    var body: some View {
+        NavigationView {
+            List { ForEach(list) { listitem in
+                CustomCell(itemName: listitem.itemName, quantity: listitem.quantity)
+            }
+                HStack {
+                    TextField("Item Name", text: $tempItemName)
+                    Spacer()
+                    TextField("Quantity", text: $tempQuantity)
+                }
+                Button("Add New Item") {
+                    if (tempQuantity != "" && tempItemName != "") {
+                        addItem()
+                    }
+                }
+                
+                Button("Delete") {
+                    deleteItem()
+                }
+            }.navigationTitle("Shopping List")
+        }
+    }
+    
+    func addItem() {
+        list.append(Items(itemName: tempItemName, quantity: tempQuantity))
+        tempItemName = ""
+        tempQuantity = ""
+    }
+    
+    func deleteItem() {
+        list.removeLast()
+    }
+
+}
+
